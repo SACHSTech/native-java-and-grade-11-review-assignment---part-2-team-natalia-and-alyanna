5dbcdde2e6058c8646ba7e567e8ddcc20c9c3716
NataliaGarciaArias <Natalia.garciaarias21@ycdsbk12.ca>
2021-05-04 18:21:09 -0400

    Method 1

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -1,9 +1,14 @@
 package gr11review.part2;
-import java.io.*;
-
 public class Utility {
 
-    
+    //Methods 1
+    public static String zipZap(String str){
 
-    
+        String strNew;
+
+        //Removes all letters between the pattern "z_p"
+        strNew = str.replaceAll("z[a-zA-Z]p", "zp");
+        
+        return strNew;
+    } 
 }
