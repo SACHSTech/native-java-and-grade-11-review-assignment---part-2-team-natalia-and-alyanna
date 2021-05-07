b665b347979d150cf4bf38b5a5decc6bab1d25a5
alyannasantos <>
2021-05-07 03:03:34 +0000

    Complete Methods 2

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -1,2 +1,13 @@
 package gr11review.part2;
 
+public class Main {
+  
+  public static void main(String[] args) {
+
+    System.out.println(Utility.sumNumbers("abc123xyz"));
+    System.out.println(Utility.sumNumbers("aa11b33"));
+    System.out.println(Utility.sumNumbers("7 11"));
+
+  }
+
+}
--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -3,7 +3,43 @@ import java.io.*;
 
 public class Utility {
 
-    
+  // Methods 2
+  public static int sumNumbers(String str) {
 
+    // Initialize variables
+    int intCharacters = str.length();
+    String strNumber = "";
+    int intSumOfNumbers = 0;
+
+    // Loop through each character of input
+    for (int intI = 0; intI < intCharacters; intI++) {
+      
+      char charCurrent = str.charAt(intI);
+
+      if (Character.isDigit(charCurrent)) {
+        strNumber = charCurrent + "";
+
+        for (int intJ = intI + 1; intJ < intCharacters; intJ++) {
+
+          char charNext = str.charAt(intJ);
+          if (Character.isDigit(charNext)) {
+            strNumber = strNumber + charNext;
+          } else {
+            break;
+          }                  
+           
+          intI = intJ;
+        } // end of inner for loop
+      } 
+
+      if (!strNumber.equals("")) {
+        intSumOfNumbers = intSumOfNumbers + Integer.parseInt(strNumber);
+        strNumber = "";
+      }
+
+    } // end of outer for loop
+
+    return intSumOfNumbers;
+  }
     
 }
