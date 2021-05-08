5a90a86dae1492daa7dde1d6d1f85bcc8f5d5de2
NataliaGarciaArias <Natalia.garciaarias21@ycdsbk12.ca>
2021-05-08 18:16:52 -0400

    testing first two problems on main

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -1,2 +1,24 @@
 package gr11review.part2;
+import java.io.*;
+import gr11review.part2.*;
 
+public class Main{
+    public static void main(String[] args) throws IOException{
+
+        //Testing Methods 1
+        String test = "zipaaaaaaaaaXzapzapzap";
+        String result;
+
+        result = Utility.zipZap(test);
+
+        System.out.println(result);
+
+        //Testing FileIO Read 1
+        String word;
+
+        word = Utility.longestWord("testfile.txt");
+
+        System.out.println(word);
+
+    }
+}
